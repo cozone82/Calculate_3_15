74693106592c71100708a6a7925e6fe4a3cde435
cltownes <cltownes@outlook.com>
2023-08-03 11:50:47 -0400

    Add project files.

---
+++ Calculate_3_15/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Calculate_3_15.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Calculate_3_15.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+
+        
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new Calculate();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculate model)
+        {
+            //*********TO USE THE COMMENTED CODE YOU MUST COMMENT OUT THE HIGHLIGHTED CODE BELOW AND THE METHODS IN THE MODEL//
+            //model.Sum = Convert.ToString("Sum is " + (model.Num1 + model.Num2));
+            //model.Product = Convert.ToString("Proudct is " + (model.Num1 * model.Num2));
+            //model.Difference = Convert.ToString("Difference is " + (model.Num1 - model.Num2));
+            //model.Quotient = Convert.ToString("Quotient is " + (model.Num1 / model.Num2));
+
+            model.Sum = Convert.ToString("Sum is " + (model.SumOfNumbers()));
+            model.Product = Convert.ToString("Proudct is " + (model.ProductOfNumbers()));
+            model.Difference = Convert.ToString("Difference is " + (model.DifferenceOfNumbers()));
+            model.Quotient = Convert.ToString("Quotient is " + (model.QuotientOfNumbers()));
+            return View(model);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Calculate_3_15/Models/Calculate.cs
@@ -0,0 +1,42 @@
+﻿namespace Calculate_3_15.Models
+{
+    public class Calculate
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public string? Sum { get; set; }
+        public string? Product { get; set; }
+        public string? Difference { get; set; }
+        public string? Quotient { get; set; }
+
+        public string SumOfNumbers()
+        {
+            string sum;
+            sum = Convert.ToString(Num1 + Num2);
+            return sum;
+        }
+
+        public string ProductOfNumbers()
+        {
+            string product;
+            product = Convert.ToString(Num1 * Num2);
+            return product;
+        }
+
+        public string DifferenceOfNumbers()
+        {
+            string difference;
+            difference = Convert.ToString(Num1 - Num2);
+            return difference;
+        }
+
+        public string QuotientOfNumbers()
+        {
+            string quotient;
+            quotient = Convert.ToString(Num1 / Num2);
+            return quotient;
+        }
+
+
+    }
+}
